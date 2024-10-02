7606968c8e6d7408a471794869bb9179f64b1ddc
why-trv <sine.rdny@gmail.com>
2024-10-02 14:22:43 +0400

    Rename CINN and CINB in 5E3 to avoid confusion with source's internal capacitance

--- src/models/tonestacks/Fender/Deluxe5E3Bright.js
+++ src/models/tonestacks/Fender/Deluxe5E3Bright.js
@@ -14,10 +14,10 @@ export class Deluxe5E3Bright extends BaseTonestack {
         RVB: 1e6,
         RVN: 1e6,            
         RL: 1e6,
-        CINN: 100e-9,
-        C1: 4.7e-9,
-        C2: 500e-12,
-        CINB: 100e-9        
+        C1: 100e-9,
+        C2: 100e-9,
+        C3: 4.7e-9,
+        C4: 500e-12             
       },
       controls: {
         RT: Tapers.LogA,
@@ -29,7 +29,7 @@ export class Deluxe5E3Bright extends BaseTonestack {
 
   calculateCoefficients(controlValues) {
     const {
-      RINN, RINB, RL, CINN, C1, C2, CINB,
+      RINN, RINB, RL, C1, C2, C3, C4,
       RT: [RT2, RT1],
       RVN: [RVN2, RVN1],
       RVB: [RVB2, RVB1]
@@ -40,45 +40,45 @@ export class Deluxe5E3Bright extends BaseTonestack {
     // Optimized operations: 232 (11.00x less)
     const t0 = RVN1 + RVN2;
     const t1 = RVB2*t0;
-    const t2 = C1*RT2;
+    const t2 = C3*RT2;
     const t3 = RT1 + RVB1;
-    const t4 = C2*t3;
+    const t4 = C4*t3;
     const t5 = t2 + t4;
-    const t6 = CINB*RVB2;
+    const t6 = C1*RVB2;
     const t7 = RL*t6;
     const t8 = t2*t4;
     const t9 = RINN*RVN2;
     const t10 = RINN + RVN2;
     const t11 = RVN1*t10;
     const t12 = t11 + t9;
-    const t13 = CINN*t12;
+    const t13 = C2*t12;
     const t14 = RVB1 + RVB2;
     const t15 = RVB2 + t0;
     const t16 = t0*t14;
     const t17 = RL*(t0 + t14) + t16;
     const t18 = RT1*RVB1;
     const t19 = RVB2*t3 + t18;
-    const t20 = C2*t19;
-    const t21 = C1*t0;
+    const t20 = C4*t19;
+    const t21 = C3*t0;
     const t22 = RINB*t14;
     const t23 = RVN2*t22;
     const t24 = RVB1*RVB2;
     const t25 = RVN1*(RINB + RVB2);
     const t26 = RINB*(RVN2 + t14) + RVB2*(RVB1 + RVN2) + t25;
-    const t27 = CINN*(RINN*t26 + RVN2*(t24 + t25) + t23);
-    const t28 = C1*t12;
+    const t27 = C2*(RINN*t26 + RVN2*(t24 + t25) + t23);
+    const t28 = C3*t12;
     const t29 = RVB2 + RVN1;
     const t30 = t15*t3 + t18;
-    const t31 = C2*(RINN*t30 + RVN2*(t18 + t29*t3));
+    const t31 = C4*(RINN*t30 + RVN2*(t18 + t29*t3));
     const t32 = t22 + t24;
     const t33 = t25*t3;
     const t34 = RINB*(t18 + t3*(RVB2 + RVN2)) + RVB2*(RVN2*t3 + t18) + t33;
-    const t35 = C2*t34;
-    const t36 = CINB*t32;
+    const t35 = C4*t34;
+    const t36 = C1*t32;
     const t37 = RVB2*t18;
     const t38 = RINB*t19;
-    const t39 = C2*(t37 + t38);
-    const t40 = CINB*t39;
+    const t39 = C4*(t37 + t38);
+    const t40 = C1*t39;
     const t41 = t0*t40 + t13*(t20 + t36);
     const t42 = RT1*RVB2;
     const t43 = RVB1*(RINB*(RT1 + RVB2) + t42);
@@ -86,17 +86,17 @@ export class Deluxe5E3Bright extends BaseTonestack {
 
     // Numerator
     const a0 = 0;
-    const a1 = CINB*RL*t1;
-    const a2 = t7*(CINN*(RINN*RVN1 + RVN2*(RINN + RVN1)) + t0*t5);
+    const a1 = C1*RL*t1;
+    const a2 = t7*(C2*(RINN*RVN1 + RVN2*(RINN + RVN1)) + t0*t5);
     const a3 = t7*(t0*t8 + t13*t5);
     const a4 = t13*t7*t8;
 
     // Denominator
     const b0 = RL*t14 + t0*(RL + t14);
-    const b1 = CINN*(RINN*t17 + RVN2*(RL*(RVN1 + t14) + RVN1*t14)) + C1*(RL*(RT2*RVB2 + RVB1*(RT2 + t0) + t0*(RT2 + RVB2)) + RT2*t16) + C2*RVB1*(RL*t15 + t1) + t17*(C2*RT1 + CINB*RINB) + t6*(RL*(RVB1 + t0) + RVB1*t0);
-    const b2 = RL*(CINN*(t14*t28 + t31) + CINB*t27 + CINB*(t21*t32 + t35) + t20*t21) + t2*(CINB*(RL*t26 + RVN2*t24 + t23) + RL*(CINN*(RINN*RVB1 + RVB2*t10 + RVN2*(RINN + RVB1) + t11) + C2*t30) + RVN1*t36 + t0*t20 + t13*t14) + t41;
-    const b3 = RL*(C1*t13*t20 + CINB*(CINN*(C2*(RINN*t34 + RVN2*t38 + RVN2*(t33 + t37)) + t28*t32) + t21*t39)) + t13*t40 + t2*(RL*(CINN*t31 + CINB*(t27 + t35)) + t41);
-    const b4 = CINN*C1*C2*CINB*(RL*t12*(RINB*t42 + t43) + RT2*(RINB*t44 + RL*(RINB*RT1*(t10*t29 + t9) + RVB1*(RINB*(t10*(RT1 + t29) + t9) + RVB2*(t10*(RT1 + RVN1) + t9)) + t44) + t12*t43));
+    const b1 = C2*(RINN*t17 + RVN2*(RL*(RVN1 + t14) + RVN1*t14)) + C3*(RL*(RT2*RVB2 + RVB1*(RT2 + t0) + t0*(RT2 + RVB2)) + RT2*t16) + C4*RVB1*(RL*t15 + t1) + t17*(C4*RT1 + C1*RINB) + t6*(RL*(RVB1 + t0) + RVB1*t0);
+    const b2 = RL*(C2*(t14*t28 + t31) + C1*t27 + C1*(t21*t32 + t35) + t20*t21) + t2*(C1*(RL*t26 + RVN2*t24 + t23) + RL*(C2*(RINN*RVB1 + RVB2*t10 + RVN2*(RINN + RVB1) + t11) + C4*t30) + RVN1*t36 + t0*t20 + t13*t14) + t41;
+    const b3 = RL*(C3*t13*t20 + C1*(C2*(C4*(RINN*t34 + RVN2*t38 + RVN2*(t33 + t37)) + t28*t32) + t21*t39)) + t13*t40 + t2*(RL*(C2*t31 + C1*(t27 + t35)) + t41);
+    const b4 = C2*C3*C4*C1*(RL*t12*(RINB*t42 + t43) + RT2*(RINB*t44 + RL*(RINB*RT1*(t10*t29 + t9) + RVB1*(RINB*(t10*(RT1 + t29) + t9) + RVB2*(t10*(RT1 + RVN1) + t9)) + t44) + t12*t43));
 
 return [
       [a0, a1, a2, a3, a4],
--- src/models/tonestacks/Fender/Deluxe5E3Normal.js
+++ src/models/tonestacks/Fender/Deluxe5E3Normal.js
@@ -12,7 +12,7 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
 
   calculateCoefficients(controlValues) {
     const {
-      RINN, RINB, RL, CINN, C1, C2, CINB,
+      RINN, RINB, RL, C1, C2, C3, C4,
       RT: [RT2, RT1],
       RVN: [RVN2, RVN1],
       RVB: [RVB2, RVB1]
@@ -22,9 +22,9 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
     // Original operations: 2501 (*, +, -)
     // Optimized operations: 278 (9.00x less)
     const t0 = RVB1 + RVB2;
-    const t1 = CINN*t0;
+    const t1 = C2*t0;
     const t2 = RL*RVN2;
-    const t3 = C1*RT2;
+    const t3 = C3*RT2;
     const t4 = t0*t3;
     const t5 = RT1*RVB2;
     const t6 = RT1 + RVB2;
@@ -32,11 +32,11 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
     const t8 = RINB + RVB2;
     const t9 = RVB1*t8;
     const t10 = t7 + t9;
-    const t11 = CINB*t10;
-    const t12 = C2*(RVB1*t6 + t5) + t11;
-    const t13 = CINN*t2;
+    const t11 = C1*t10;
+    const t12 = C4*(RVB1*t6 + t5) + t11;
+    const t13 = C2*t2;
     const t14 = RINB*t5;
-    const t15 = C2*CINB;
+    const t15 = C4*C1;
     const t16 = RINB*RVB1;
     const t17 = RT1*t16;
     const t18 = RINB + RVB1;
@@ -48,15 +48,15 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
     const t24 = RT1*t0;
     const t25 = RVB1*RVB2;
     const t26 = RINN*RVB1;
-    const t27 = C2*RL;
+    const t27 = C4*RL;
     const t28 = t27*(t24 + t25);
     const t29 = RL + RVB2;
     const t30 = RT1*(RL + t0) + RVB1*t29;
     const t31 = RL*RVB2;
-    const t32 = C2*t30 + CINB*(RINB*t29 + t31 + t9);
+    const t32 = C4*t30 + C1*(RINB*t29 + t31 + t9);
     const t33 = RL*t11 + t28;
     const t34 = RL*t4;
-    const t35 = C1*(RT2*t29 + RVB1*(RL + RT2) + t31) + t32;
+    const t35 = C3*(RT2*t29 + RVB1*(RL + RT2) + t31) + t32;
     const t36 = RT1*RVB1;
     const t37 = RT1 + RVB1;
     const t38 = RVB2*t37;
@@ -64,9 +64,9 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
     const t40 = RINN*RVN1;
     const t41 = RINN + RVN1;
     const t42 = RVN2*t41 + t40;
-    const t43 = CINN*t42;
+    const t43 = C2*t42;
     const t44 = RT1 + t41;
-    const t45 = CINN*C1;
+    const t45 = C2*C3;
     const t46 = t43*(RVB2*t18 + t16);
     const t47 = RINB*t37;
     const t48 = RVB2*(t36 + t47) + t17;
@@ -86,9 +86,9 @@ export class Deluxe5E3Normal extends Deluxe5E3Bright {
 
     // Denominator
     const b0 = RL*t19 + t0*(RL + t19);
-    const b1 = CINB*RINB*t0*t19 + RL*(CINN*(RINN*RVB2 + RVN2*(RINN + t0) + t22 + t26) + C1*(RT2*RVB1 + RT2*RVB2 + t19*(RT2 + t0)) + C2*(RT1*(t0 + t19) + RVB1*(RVB2 + t19)) + CINB*(t7 + t8*(RVB1 + t19))) + t1*t23 + t19*(C2*t24 + t25*(C2 + CINB) + t4);
-    const b2 = CINN*(RINN*t34 + RINN*(t19*t35 + t33) + RVN2*(RVN1*t35 + t33 + t34)) + RL*t15*(RT1*t10 + RVB1*t7) + t19*(C1*t28 + CINB*(C1*RL*t10 + C2*(RINB*t30 + RVB2*(RL*RVB1 + RT1*(RL + RVB1))))) + t3*(t19*t32 + t33);
-    const b3 = C2*t3*t39*t43 + CINB*(C2*t48*(t19*t3 + t43) + RL*(CINN*t3*(RINB*t49 + RVB2*(RINB*t21 + t49)) + C1*t46 + C2*(CINN*RINB*t51 + CINN*RVB2*(t51 + t52) + C1*t19*t48 + t3*(RINB*(RT1*t19 + RVB1*(RT1 + t19)) + RVB2*(RT1*RVN2 + RVB1*t19 + t47 + t50)))) + t3*t46) + t27*t45*(RT2*(RT1*t42 + RVB1*(RINN*RT1 + RVN2*t44 + t40) + t21*t38) + t39*t42);
+    const b1 = C1*RINB*t0*t19 + RL*(C2*(RINN*RVB2 + RVN2*(RINN + t0) + t22 + t26) + C3*(RT2*RVB1 + RT2*RVB2 + t19*(RT2 + t0)) + C4*(RT1*(t0 + t19) + RVB1*(RVB2 + t19)) + C1*(t7 + t8*(RVB1 + t19))) + t1*t23 + t19*(C4*t24 + t25*(C4 + C1) + t4);
+    const b2 = C2*(RINN*t34 + RINN*(t19*t35 + t33) + RVN2*(RVN1*t35 + t33 + t34)) + RL*t15*(RT1*t10 + RVB1*t7) + t19*(C3*t28 + C1*(C3*RL*t10 + C4*(RINB*t30 + RVB2*(RL*RVB1 + RT1*(RL + RVB1))))) + t3*(t19*t32 + t33);
+    const b3 = C4*t3*t39*t43 + C1*(C4*t48*(t19*t3 + t43) + RL*(C2*t3*(RINB*t49 + RVB2*(RINB*t21 + t49)) + C3*t46 + C4*(C2*RINB*t51 + C2*RVB2*(t51 + t52) + C3*t19*t48 + t3*(RINB*(RT1*t19 + RVB1*(RT1 + t19)) + RVB2*(RT1*RVN2 + RVB1*t19 + t47 + t50)))) + t3*t46) + t27*t45*(RT2*(RT1*t42 + RVB1*(RINN*RT1 + RVN2*t44 + t40) + t21*t38) + t39*t42);
     const b4 = t15*t45*(RL*(RT2*(RINB*t54 + RVB2*(t52 + t54)) + RVB2*t23*(RINB*RT1 + RVB1*(RINB + RT1)) + t16*t53) + RT2*t23*(RVB1*(RT1*t8 + t7) + t14));
 
 return [
